5d40415153504895e45cd10a158de75343fd430f
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-10-20 19:32:11 +0300

    security & lombok

--- src/main/java/com/api/movie/MovieApplication.java
+++ src/main/java/com/api/movie/MovieApplication.java
@@ -1,13 +1,28 @@
 package com.api.movie;
 
+import com.api.movie.exceptions.ResourceNotFoundException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class MovieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieApplication.class, args);
-	}
+		Scanner scanner = new Scanner(System.in);
 
+		// Prompt user for password
+		System.out.print("Enter password: ");
+		String inputPassword = scanner.nextLine();
+
+		// Validate password
+		if ("123".equals(inputPassword)) {
+			SpringApplication.run(MovieApplication.class, args);
+		} else {
+			throw new ResourceNotFoundException("Not admin - access denied.");
+		}
+
+		scanner.close();
+	}
 }
